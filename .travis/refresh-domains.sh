f3bf2a51f1fe2919d0f44d9776fe0c9f3f2183b2
Ando Roots <ando@sqroot.eu>
2022-09-20 21:58:34 +0300

    Replace travis with github actions

--- .travis/refresh-domains.sh
+++ .travis/refresh-domains.sh
@@ -2,37 +2,17 @@
 
 set -e
 
-if [ $TRAVIS_EVENT_TYPE = "cron" ]; then
-  echo "Build triggered by CRON, updating domain DB"
+echo "Refreshing domain list"
+./cron/compile-domain-list.sh
 
-  echo "Setting up Git..."
-  # Setup Git and SSH push access to upstream repo
-  chmod 600 .travis/ssh-key
-  eval $(ssh-agent -s)
-  ssh-add .travis/ssh-key > /dev/null
-  git config user.email "travis@sqroot.eu"
-  git config user.name "Travis CI"
-  git remote add gh git@github.com:anroots/ee-domains.git
-  git checkout -f master
-  git pull gh master
-  
-  echo "Git set up"
-  echo "Refreshing domain list"
-  ./cron/compile-domain-list.sh
+echo "Adding changes to Git"
+git status
+# Commit changes
+added=$(wc -l src/lists/added.txt | cut -f 1 -d ' ')
+deleted=$(wc -l src/lists/deleted.txt | cut -f 1 -d ' ')
+git add src/lists
+git commit -m "CI CRON: Update domain lists for $(date  --rfc-3339=date) (${added} added; ${deleted} deleted)"
+echo "Pushing changes..."
+git push gh master
 
-  echo "Adding changes to Git"
-  git status
-  # Commit changes
-  added=$(wc -l src/lists/added.txt | cut -f 1 -d ' ')
-  deleted=$(wc -l src/lists/deleted.txt | cut -f 1 -d ' ')
-  git add src/lists
-  git commit -m "CI CRON: Update domain lists for $(date  --rfc-3339=date) (${added} added; ${deleted} deleted)"
-  echo "Pushing changes..."
-  git push gh master
-
-  shred -fu .travis/ssh-key
-  echo "Done updating domain DB"
-else
-  echo "Build was triggered by a Git push, not going to refresh domain lists"
-  exit 0
-fi
+echo "Done updating domain DB"
--- cron/compile-domain-list.sh
+++ cron/compile-domain-list.sh
@@ -15,10 +15,6 @@ echo "Parsing domains from the zone file..."
 # There are Many domain names in the zone, exactly 33 characters long. They don't WHOIS. Not sure what they are. `grep {35}` filters them out for noise reduction
 grep "^[^;]"  zone.ee | cut -f 1 | cut -f 1 -d ' ' | sed 's/\.$//' | grep '.ee' | grep -vE '^.{35}$' | uniq | sort > domains.new.txt
 
-
-# Get the "old" copy of domains.txt...
-curl --silent https://ee-domains.sqroot.eu/lists/domains.txt | uniq | sort > domains.txt
-
 # Find diffs
 comm -23 domains.txt domains.new.txt > deleted.txt
 comm -13 domains.txt domains.new.txt > added.txt
