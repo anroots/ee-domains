8c5cea3ec73736a841834a8713a929ae6952813e
Ando Roots <ando@sqroot.eu>
2023-12-14 16:15:12 +0200

    Use dist/ for deployment

--- build.sh
+++ build.sh
@@ -8,9 +8,5 @@ cp public/lists/last-update.json data/last-update.json
 
 npx nuxi generate
 
-# CloudFlare Pages can not handle paths beginning with a dot for deployment
-# (internal check for file existence fails)
-mv .output/public output
-
-echo "Finished building site, output is at output/public"
-ls -la output
+echo "Finished building site, output is at dist"
+ls -la dist
