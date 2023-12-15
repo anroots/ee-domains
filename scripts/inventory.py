13090d5e74c6fdde30e17e8060c2b838d4c09180
Ando Roots <ando@sqroot.eu>
2023-12-15 14:18:31 +0200

    Improve logging

--- scripts/inventory.py
+++ scripts/inventory.py
@@ -19,7 +19,7 @@ def track_progress(future):
     global done_progress
     global total_records
     done_progress += 1
-    if done_progress % 10 == 0:
+    if done_progress % 500 == 0:
         seconds = round(time.time() - start_time)
         click.echo(f'{done_progress} of {total_records} done in {seconds}s')
     results.put(future.result())
@@ -37,7 +37,7 @@ def main(domains_file, inventory_file, workers, verify_proxy):
 
         http_proxy = os.environ.get('HTTP_PROXY')
         https_proxy = os.environ.get('HTTPS_PROXY')
-        click.echo(f'HTTP_PROXY={http_proxy}; HTTPS_PROXY={https_proxy}')
+        click.echo(f'Running with proxy settings: HTTP_PROXY={http_proxy}; HTTPS_PROXY={https_proxy}')
         
         proxy_info = requests.get('https://api.myip.com/')
         proxy_info.raise_for_status()
@@ -45,6 +45,7 @@ def main(domains_file, inventory_file, workers, verify_proxy):
         if not cc or verify_proxy != cc:
             click.secho(f'Proxy test failed, actual CC is {cc}, expected CC was {verify_proxy}', fg='red')
             sys.exit(1)
+        click.secho(f'Verified proxy settings, reported CC is {cc}', fg='green')
 
     global start_time
     global total_records
@@ -59,14 +60,13 @@ def main(domains_file, inventory_file, workers, verify_proxy):
         with open(domains_file, 'r') as f:
             for domain in f:
                 future = executor.submit(scan_domain, domain.strip())
+                future.add_done_callback(track_progress)
                 futures.append(future)
                 total_records += 1
 
-    click.echo(f'Added {total_records} domains to the queue')
-    results_writer.start()
+        click.echo(f'Added {total_records} domains to the queue')
+        results_writer.start()
     
-    for future in concurrent.futures.as_completed(futures):
-        track_progress(future)
     results.put(None)
     results.join()
     results_writer.join()
